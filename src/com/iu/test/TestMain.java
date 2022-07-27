60e1c7add972004878d34f6555bc23d3ab70cb42
songjh11 <jhmelody91@gmail.com>
2022-07-27 12:20:03 +0900

    connection 완

--- src/com/iu/test/TestMain.java
+++ src/com/iu/test/TestMain.java
@@ -1,8 +1,24 @@
 package com.iu.test;
 
+import java.sql.SQLException;
+
+import com.iu.util.DBConnector;
+
 public class TestMain {
 
 	public static void main(String[] args) {
+		
+		DBConnector dbc = new DBConnector();
+		
+		try {
+			dbc.getConnection();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
